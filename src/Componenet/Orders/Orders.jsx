11a71aff0c51ac0d97ac1bc157a1cc61b602bf3b
girmaytajebe <147225188+girmaytajebe@users.noreply.github.com>
2024-02-26 22:59:45 -0600

    update

--- src/Componenet/Orders/Orders.jsx
+++ src/Componenet/Orders/Orders.jsx
@@ -5,38 +5,38 @@ import "./Orders.css";
 import { useStateValue } from "../StateProvider/StateProvider";
 
 function Orders() {
-	const [{ baskek, user }, dispatch] = useStateValue();
-	const [orders, setOrders] = useState([]);
+  const [{ baskek, user }, dispatch] = useStateValue();
+  const [orders, setOrders] = useState([]);
 
-	useEffect(() => {
-		if (user) {
-			db.collection("users")
-				.doc(user?.uid)
-				.collection("orders")
-				.orderBy("created", "desc")
-				.onSnapshot((snapshot) =>
-					setOrders(
-						snapshot.docs.map((doc) => ({
-							id: doc.id,
-							data: doc.data(),
-						}))
-					)
-				);
-		} else {
-			setOrders([]);
-		}
-	}, [user]);
+  useEffect(() => {
+    if (user) {
+      db.collection("users")
+        .doc(user?.uid)
+        .collection("orders")
+        .orderBy("created", "desc")
+        .onSnapshot((snapshot) =>
+          setOrders(
+            snapshot.docs.map((doc) => ({
+              id: doc.id,
+              data: doc.data(),
+            }))
+          )
+        );
+    } else {
+      setOrders([]);
+    }
+  }, [user]);
 
-	return (
-		<div className="orders">
-			<h1>Your Orders</h1>
-			<div className="orders__order">
-				{orders?.map((order, index) => (
-					<Order key={index} order={order} />
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="orders">
+      <h1>Your Orders</h1>
+      <div className="orders__order">
+        {orders?.map((order, index) => (
+          <Order key={index} order={order} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Orders;
